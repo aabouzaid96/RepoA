b7a767ab92df879e9535c038cefa9f65e123b0c5
abouzaid <a.abouzaid@aems.ai>
2024-12-26 13:08:56 +0200

    update Repoa A to 3

--- add_users/views.py
+++ add_users/views.py
@@ -20,4 +20,4 @@ class AddUserView(APIView):
 
 
 def sample_view(request):
-    return JsonResponse({"message": "Hello from RepoA - updated 2"})
+    return JsonResponse({"message": "Hello from RepoA - updated 3"})
