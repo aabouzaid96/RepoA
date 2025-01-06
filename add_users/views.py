95912983b801b24b47d90d11ef584618391f2473
abouzaid <a.abouzaid@aems.ai>
2025-01-06 14:12:28 +0200

    add code that has error

--- add_users/views.py
+++ add_users/views.py
@@ -20,4 +20,4 @@ class AddUserView(APIView):
 
 
 def sample_view(request):
-    return JsonResponse({"message": "Hello from RepoA - add_users!"})
+    return JsonResponse({"message": "Hello from RepoA - add_users!"})..
