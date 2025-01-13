1bfa99d45790b05e7c399dae092fed316445760e
abouzaid <a.abouzaid@aems.ai>
2025-01-13 10:28:34 +0200

    update Repo A api

--- add_users/views.py
+++ add_users/views.py
@@ -20,4 +20,4 @@ class AddUserView(APIView):
 
 
 def sample_view(request):
-    return JsonResponse({"message": "Hello from RepoA - Change 3 Friday"})
+    return JsonResponse({"message": "Hello from RepoA - Demo Day Monday 1"})
